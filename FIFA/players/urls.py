ac6c95ac54d5c4b1ccad0033eabdffb4026448bc
Cristina Acevedo <62355692+CTRLCAT@users.noreply.github.com>
2021-02-08 13:20:05 -0500

    Added a database

--- FIFA/players/urls.py
+++ FIFA/players/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path
 from players import views
+from players.models import LogMessage
+
+home_list_view = views.HomeListView.as_view(
+    queryset=LogMessage.objects.order_by("-log_date")[:5],  # :5 limits the results to the five most recent
+    context_object_name="message_list",
+    template_name="players/home.html",
+)
 
 urlpatterns = [
-    path('',views.home,name='home'),
+    path("", home_list_view, name="home"),
     path('players/<name>', views.player_view, name='player_page'),
     path('contact/', views.contact_view, name='contact_page'),
     path('about/', views.about_view, name='about_page'),
     path('home/', views.home_view, name='home_page'),
+    path('log/', views.log_message_view, name='log_page')
 ]
+
+
--- FIFA/players/views.py
+++ FIFA/players/views.py
@@ -2,10 +2,22 @@ import re
 from django.utils.timezone import datetime
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.shortcuts import redirect
+from players.forms import LogMessageForm
+from players.models import LogMessage
+from django.views.generic import ListView
 
 # Create your views here.
-def home(request):
-    return HttpResponse("Players should b here")
+class HomeListView(ListView):
+    """Renders the home page, with a list of all messages."""
+    model = LogMessage
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeListView, self).get_context_data(**kwargs)
+        return context
+
+# def home(request):
+#     return HttpResponse("Hello, Django!")
 
 def player_view(request, name):
     return render(
@@ -25,3 +37,15 @@ def home_view(request):
 
 def about_view(request):
     return render(request, 'players/about.html')
+
+def log_message_view(request):
+    form = LogMessageForm(request.POST or None)
+
+    if request.method == "POST":
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.log_date = datetime.now()
+            message.save()
+            return redirect("home")
+    else:
+        return render(request, "players/log_message.html", {"form": form})
