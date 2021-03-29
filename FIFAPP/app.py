ffa909e6726cdc7b22879066b688440974284d5a
Cristina Acevedo <62355692+CTRLCAT@users.noreply.github.com>
2021-03-29 18:51:10 -0500

    Connected app to partial database

--- FIFAPP/app.py
+++ FIFAPP/app.py
@@ -1,42 +1,23 @@
 from flask import Flask, render_template
+from datetime import datetime
 import json
-import mariadb
-
-app.config["DEBUG"] = True
-
-config = {
-    'host': '127.0.0.1',
-    'port': 3306,
-    'user': 'root',
-    'password': 'Password123!',
-    'database': 'demo'
-}
+from flask_sqlalchemy import SQLAlchemy
 
 app =Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///Players.db'
+db=SQLAlchemy(app)
+
+players=db.Table('players21',db.metadata,autoload=True, autoload_with=db.engine)
+
 @app.route('/')
 def main():
-    return render_template('index.html')
+    results=db.session.query(players).all()
+    for r in results:
+        print(r.last_name)
+    return ''
+    #return render_template('index.html')
 
-# route to return all people
-@app.route('/api/people', methods=['GET'])
-def index():
-   # connection for MariaDB
-   conn = mariadb.connect(**config)
-   # create a connection cursor
-   cur = conn.cursor()
-   # execute a SQL statement
-   cur.execute("select * from people")
-
-   # serialize results into JSON
-   row_headers=[x[0] for x in cur.description]
-   rv = cur.fetchall()
-   json_data=[]
-   for result in rv:
-        json_data.append(dict(zip(row_headers,result)))
-
-   # return the results!
-   return json.dumps(json_data)
 
 if __name__ == '__main__':
     app.run()
