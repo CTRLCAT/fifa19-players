2e7df6a59b39d67783288d2e01abb5cee1ec492b
Cristina Acevedo <62355692+CTRLCAT@users.noreply.github.com>
2021-03-30 19:47:52 -0500

    Created base html and stylesheet

--- FIFAPP/app.py
+++ FIFAPP/app.py
@@ -3,21 +3,29 @@ from datetime import datetime
 import json
 from flask_sqlalchemy import SQLAlchemy
 
+
 app =Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///Players.db'
 db=SQLAlchemy(app)
 
-players=db.Table('players21',db.metadata,autoload=True, autoload_with=db.engine)
+Players=db.Table('players21',db.metadata,autoload=True, autoload_with=db.engine)
 
 @app.route('/')
 def main():
-    results=db.session.query(players).all()
+    results=db.session.query(Players).all()
     for r in results:
         print(r.last_name)
-    return ''
-    #return render_template('index.html')
 
+    return render_template('index.html')
+
+@app.route('/base')
+def base():
+    #results=db.session.query(Players).all()
+    #for r in results:
+    #    print(r.last_name)
+
+    return render_template('base.html')
 
 if __name__ == '__main__':
     app.run()
